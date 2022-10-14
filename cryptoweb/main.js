018482514b19907391541fa07744acbcc8d6fa3b
KadhamVj23 <vijayalakshmikadham@gmail.com>
2022-10-14 20:10:50 +0530

    crypto web for seeing the price of crypto coins

---
+++ cryptoweb/main.js
@@ -0,0 +1,17 @@
+let btc = document.getElementById("bitcoin");
+let eth = document.getElementById("ethereum");
+let doge = document.getElementById("dogecoin");
+
+let settings = {
+    "async": true,
+    "scrossDomain": true,
+    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "method": "GET",
+    "headers":{}
+};
+$.ajax(settings).done(function (response){
+    console.log(response);
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+});
